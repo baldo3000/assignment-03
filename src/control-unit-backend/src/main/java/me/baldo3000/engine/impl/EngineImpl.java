ae28bda36357f2cfd1a82219e3dec2b77bb7f288
Andrea Baldazzi <andrea.baldazzi7@studio.unibo.it>
2025-02-03 23:27:19 +0100

    CU: save reported temperature instead of strings

--- src/control-unit-backend/src/main/java/me/baldo3000/engine/impl/EngineImpl.java
+++ src/control-unit-backend/src/main/java/me/baldo3000/engine/impl/EngineImpl.java
@@ -7,34 +7,43 @@ import me.baldo3000.model.impl.MQTTAgentImpl;
 
 public class EngineImpl implements Engine {
 
-    final Vertx vertx;
-    final MQTTAgent mqttAgent;
+    private final Vertx vertx;
+    private final MQTTAgent mqttAgent;
 
     private State state;
     private long stateTimestamp;
     private boolean justEntered;
-    private String latestMQTTMessage;
+    private double latestReportedTemperature;
 
     public EngineImpl() {
         this.vertx = Vertx.vertx();
         this.mqttAgent = new MQTTAgentImpl();
-        this.vertx.deployVerticle(this.mqttAgent);
-        vertx.eventBus().consumer("mqtt-messages", message -> {
-            this.latestMQTTMessage = message.body().toString();
-            System.out.println("Received message: " + this.latestMQTTMessage);
-        });
     }
 
     @Override
     public void initialize() {
+        this.vertx.deployVerticle(this.mqttAgent);
+        this.vertx.eventBus().consumer("mqtt-messages", message -> {
+            final String payload = message.body().toString();
+            System.out.println("Received message: " + payload);
+            if (payload.length() >= 3) {
+                final String prefix = payload.substring(0, 3);
+                if (prefix.equals("ts:")) {
+                    try {
+                        final double temperature = Double.parseDouble(payload.substring(3));
+                        this.latestReportedTemperature = temperature;
+                        System.out.println("Temperature: " + temperature);
+                    } catch (final NumberFormatException ignored) {
+                    }
+                }
+            }
+        });
         setState(State.NORMAL);
     }
 
     @Override
     public void mainLoop() {
         while (true) {
-            // final String latestMessage = this.mqttAgent.getLatestMessage();
-            // System.out.println("Latest message: " + latestMessage);
             switch (this.state) {
                 case NORMAL -> {
                     if (doOnce()) {
